0ad51d85f7e1e43a3a30bbfbde120c79043d6d77
lhy <984038622@qq.com>
2023-03-06 11:16:52 +0800

    暂时不展示后面模块

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -17,37 +17,37 @@ module.exports = [
       { text: 'html',link:"/html/"},
     ],
   },
-  {
-    text: '收藏',
-    link: '/pages/b4e632/',
-    items: [
-      { text: '文章', link: '/pages/2c9c8d/' },
-      { text: '视频', link: '/pages/e3d44a/' },
-      { text: '音乐', link: '/pages/7fbfa7/' },
-    ],
-  },
-  {
-    text: '喜欢的',
-    link: '/love/',
-    items: [
-      { text: '02.游戏',
-         link: '/pages/cd90ce/' ,
-         items:[
-          { text: '只狼', link: '/pages/cd90ce/' },
-          { text: '老头环', link: '/pages/c64d41/' }
-         ]
-      },      
-      { 
-        text: '与她相关', 
-        link: '/pages/7d6a40/' ,
-        items:[
-          { text: '相遇', link: '/pages/7d6a41/' },
-          { text: '相识', link: '/pages/7d6a42/' },
-          { text: '未完待续', link: '/pages/7d6a43/' },
-        ]
-     }
-    ],
-  },
+  // {
+  //   text: '收藏',
+  //   link: '/pages/b4e632/',
+  //   items: [
+  //     { text: '文章', link: '/pages/2c9c8d/' },
+  //     { text: '视频', link: '/pages/e3d44a/' },
+  //     { text: '音乐', link: '/pages/7fbfa7/' },
+  //   ],
+  // },
+  // {
+  //   text: '喜欢的',
+  //   link: '/love/',
+  //   items: [
+  //     { text: '02.游戏',
+  //        link: '/pages/cd90ce/' ,
+  //        items:[
+  //         { text: '只狼', link: '/pages/cd90ce/' },
+  //         { text: '老头环', link: '/pages/c64d41/' }
+  //        ]
+  //     },
+  //     {
+  //       text: '与她相关',
+  //       link: '/pages/7d6a40/' ,
+  //       items:[
+  //         { text: '相遇', link: '/pages/7d6a41/' },
+  //         { text: '相识', link: '/pages/7d6a42/' },
+  //         { text: '未完待续', link: '/pages/7d6a43/' },
+  //       ]
+  //    }
+  //   ],
+  // },
   {
     text: '索引',
     link: '/archives/',
