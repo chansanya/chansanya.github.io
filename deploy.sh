a3ef9b1110cf66c44a8ebdc5685ebecedb6d40f6
server <984038622@qq.com>
2023-03-09 10:52:50 +0800

    修复发布脚本

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,10 @@ git pull
 # 生成文档
 yarn run build
 
+rm -rf  /opt/docker/nginx/html/dist/*
+echo 删除当前版本文件
 mv -f ./docs/.vuepress/dist  /opt/docker/nginx/html
+echo 移动新版本文件至目标路径
 rm -rf ./docs/.vuepress/dist
+echo 删除打包文件
+echo 操作成功
