c67002ad330415fdc3a6ba04dff27ea93c4753fd
lhy <984038622@qq.com>
2023-03-09 10:43:15 +0800

    修改部署脚本

--- deploy.sh
+++ deploy.sh
@@ -6,26 +6,5 @@ set -e
 # 生成文档
 yarn run build
 
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
-# 如果是发布到自定义域名
-#echo 'blog.lovefile.com' > CNAME
-
-if [ -z "$GIT_PAGES_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com:chansanya/chansanya.github.io.git
-else
-  msg='来自github action的自动部署'
-  githubUrl=https://chansanya:${GIT_PAGES_TOKEN}@github.com/chansanya/blog.git
-  git config --global user.name "yan"
-  git config --global user.email "984038622@qq.com"
-fi
-git init
-git add -A
-git commit -m "${msg}"
-git push -f $githubUrl  master:gh-pages
-
-cd -
-
-rm -rf docs/.vuepress/dist
+mv -f ./docs/.vuepress/dist  /opt/docker/nginx/html
+rm -rf ./docs/.vuepress/dist
