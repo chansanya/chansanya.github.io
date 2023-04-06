5a7d73c8d858de938a7dd41bfd0c099dbe41c33c
lhy <984038622@qq.com>
2023-04-06 10:18:26 +0800

    发布

--- upload.sh
+++ upload.sh
@@ -10,7 +10,7 @@ echo 上传新版本文件至目标路径
 scp -P 22 -r  ./docs/.vuepress/dist/ root@1.116.69.228:/opt/docker/nginx/html
 
 
-scp -P 22 -r  ./docs/.vuepress/dist/ root@43.154.116.232:/usr/tools/blog
+scp -P 22 -r  ./docs/.vuepress/dist/ root@43.154.249.166:/usr/tools/blog
 
 rm -rf ./docs/.vuepress/dist
 echo 删除打包文件
