1a7140cc5ea96bd7af74ee42fdae6e8fb03b0aab
lhy <984038622@qq.com>
2023-08-11 11:26:18 +0800

    添加看板娘

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,6 @@
 const plugins = require('./config/plugins.js');
 const themeConfig = require('./config/themeConfig.js');
+const head = require('./config/head.js');
 
 module.exports = {
   theme: 'vdoing',
@@ -9,31 +10,10 @@ module.exports = {
   base: '/blog/',
   // 主题配置
   themeConfig,
-
   // 注入到页面<head>中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
-  head: [
-    ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
-    [
-      'meta',
-      {
-        name: 'keywords',
-        content: 'Java,Linux',
-      },
-    ],
-    ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
-    [
-      'script',
-      {
-        'data-ad-client': 'ca-pub-7828333725993554',
-        async: 'async',
-        src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
-      },
-    ], // 网站关联Google AdSense 与 html格式广告支持（你可以去掉）
-  ],
-
+  head,
   // 插件配置
   plugins,
-
   markdown: {
     // MD代码行数显示
     // lineNumbers: true,
---
+++ docs/.vuepress/config/head.js
@@ -0,0 +1,29 @@
+module.exports =[
+    ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
+    [
+        'meta',
+        {
+            name: 'keywords',
+            content: 'Java,Linux',
+        },
+    ],
+    // 移动浏览器主题颜色
+    ['meta', { name: 'theme-color', content: '#11a8cd' }],
+    // 网站关联Google AdSense 与 html格式广告支持（你可以去掉）
+    [
+        'script',
+        {
+            'data-ad-client': 'ca-pub-7828333725993554',
+            async: 'async',
+            src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+        },
+    ],
+    [
+        'script',
+        {
+            async: 'async',
+            src: 'https://fastly.jsdelivr.net/gh/chansanya/live2d-widget@latest/autoload.js',
+        },
+    ],
+
+]
