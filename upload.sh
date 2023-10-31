d68943f0638ef20497839525d5e03f4d6d29120e
lhy <984038622@qq.com>
2023-10-31 14:30:43 +0800

    更新域名

--- upload.sh
+++ upload.sh
@@ -9,9 +9,6 @@ yarn run build
 echo 上传新版本文件至目标路径
 scp -P 22 -r  ./docs/.vuepress/dist/ root@1.116.69.228:/opt/docker/nginx/html
 
-
-scp -P 22 -r  ./docs/.vuepress/dist/ root@43.154.203.9:/usr/tools/blog
-
 rm -rf ./docs/.vuepress/dist
 echo 删除打包文件
 echo 操作成功
